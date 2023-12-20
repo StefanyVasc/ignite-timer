01c4f9f61a43f7776741516a9d9fa832b525ce18
StefanyVasc <stefany.vasc.sa@gmail.com>
2023-12-19 22:55:49 -0300

    style: add global styles and button style

---
+++ src/@types/styles.d.ts
@@ -0,0 +1,8 @@
+import "styled-components"
+import { defaultTheme } from "../components/styles/themes/default"
+
+type ThemeType = typeof defaultTheme
+
+declare module "styled-components" {
+  export interface DefaultTheme extends ThemeType {}
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { ThemeProvider } from "styled-components"
+import { Button } from "./components/Button"
+import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from "./styles/themes/default"
+
 export function App() {
   return (
-    <>
-      <h1>clean up</h1>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="danger" />
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
