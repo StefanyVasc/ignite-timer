f0d382b19df8a12166c17938bdfc5b0ef2afb467
StefanyVasc <stefany.vasc.sa@gmail.com>
2023-12-20 00:07:00 -0300

    chore: add eslint and config it

--- src/@types/styles.d.ts
+++ src/@types/styles.d.ts
@@ -1,8 +1,8 @@
-import "styled-components"
-import { defaultTheme } from "../components/styles/themes/default"
+import 'styled-components'
+import { defaultTheme } from '../components/styles/themes/default'
 
 type ThemeType = typeof defaultTheme
 
-declare module "styled-components" {
+declare module 'styled-components' {
   export interface DefaultTheme extends ThemeType {}
 }
