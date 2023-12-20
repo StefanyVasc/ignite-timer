86c954ae880b6f72787657898ba2ca0d64f2642d
StefanyVasc <stefany.vasc.sa@gmail.com>
2023-12-20 11:33:32 -0300

    feat: create router files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="danger" />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
