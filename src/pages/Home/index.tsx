7cbb5abed548c1aacc73a68a064d7617133519bc
StefanyVasc <stefany.vasc.sa@gmail.com>
2023-12-27 20:11:00 -0300

    feat: update title w/ countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -47,12 +47,17 @@ export function Home() {
   })
 
   useEffect(() => {
+    let countdownInterval: number | undefined
     if (activeCycle) {
-      setInterval(() => {
+      countdownInterval = setInterval(() => {
         setAmountSecondPast(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
+    }
+
+    return () => {
+      clearInterval(countdownInterval)
     }
   }, [activeCycle])
 
@@ -68,6 +73,8 @@ export function Home() {
 
     setCycles((currentCycles) => [...currentCycles, newCycle])
     setActiveCycleId(id)
+    setAmountSecondPast(0)
+
     reset()
   }
 
@@ -79,6 +86,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
 
   const task = watch('task')
   const isSubmitDisabled = !task
